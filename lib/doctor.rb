02136d21f30ca1efa4c9f00b64cc61636a69e6d3
cooljasonmelton <cooljasonmelton>
2020-02-24 14:51:07 -0600

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,34 @@
+require_relative 'appointment.rb'
+class Doctor 
+    attr_accessor :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def appointments
+        Appointment.all.select do |app|
+            app.doctor == self 
+        end     
+    end 
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+
+    end 
+
+    def patients
+        self.appointments.map do |app|
+            app.patient
+        end
+    end 
+
+
+end 
+
